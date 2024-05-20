69d2ac720883e4c4f4b7913a06e7f1f1826955f1
Nicolas <nicolascarobene1@gmail.com>
2024-05-20 07:22:04 +0200

    add viewTransition

---
+++ src/utils/viewTransition.ts
@@ -0,0 +1,12 @@
+import { onNavigate } from "$app/navigation";
+
+onNavigate((navigation) => {
+  if (!document.startViewTransition) return;
+
+  return new Promise((resolve) => {
+    document.startViewTransition(async () => {
+      resolve();
+      await navigation.complete;
+    });
+  });
+});
